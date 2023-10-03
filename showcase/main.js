4d7ff076a286dd742c74f50f6567e8d1a89f131c
GrakePCH <grakepch@gmail.com>
2023-10-03 15:01:56 -0400

    Init

--- showcase/main.js
+++ showcase/main.js
@@ -1,16 +1,43 @@
-import {completeIconSet} from '../dist/icons';
+import {completeIconSet} from '../dist/icons/build/complete-icon-set';
 
 function buildIconCard(icon) {
     const iconCard = document.createElement('div');
     iconCard.classList.add('icon-card');
     iconCard.appendChild(buildSVGElement(icon));
-    iconCard.appendChild(document.createTextNode(icon.name));
+    iconCard.appendChild(document.createTextNode(icon.name.split("_").join(" ")));
     return iconCard;
 }
 
 function buildSVGElement(icon) {
     const div = document.createElement('DIV');
     div.innerHTML = icon.data;
+    if (!div.childNodes[0].getAttribute("viewBox")) {
+        div.childNodes[0].setAttribute("viewBox", "0 0 400 400");
+    }
+    for (const e of div.childNodes[0].childNodes) {
+
+        for (const e_child of e.childNodes) {
+            try {
+                if (!e_child.getAttribute("class")) continue;
+            } catch {
+                continue;
+            }
+            var classList = e_child.getAttribute("class").split(" ")
+            const idx = classList.indexOf("st0");
+            if (idx > -1) classList.splice(idx, 1);
+            const idx2 = classList.indexOf("st1");
+            if (idx2 > -1) classList.splice(idx2, 1);
+            e_child.setAttribute("class", classList.join(" "));
+        }
+
+        if (!e.getAttribute("class")) continue;
+        var classList = e.getAttribute("class").split(" ")
+        const idx = classList.indexOf("st0");
+        if (idx > -1) classList.splice(idx, 1);
+        const idx2 = classList.indexOf("st1");
+        if (idx2 > -1) classList.splice(idx2, 1);
+        e.setAttribute("class", classList.join(" "));
+    }
     return (
         div.querySelector('svg') ||
         this.document.createElementNS('http://www.w3.org/2000/svg', 'path')
